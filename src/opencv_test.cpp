1fbe4c342653ba4c7e850c850422bb7eb388a74b
zhaoqiangwei <zhaoqiangwei2002@163.com>
2021-03-28 10:33:31 +0800

    test opencv cmake

---
+++ src/opencv_test.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <opencv2/imgproc/types_c.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        cout << "Usage: opencv_test <image path>" << endl;
+        return -1;
+    }
+
+    char *imgName = argv[1];
+    Mat image;
+
+    image = imread(imgName, 1);
+    if (!image.data)
+    {
+        cout << "No image data" << endl;
+        return -1;
+    }
+    Mat gray_img;
+
+    cvtColor(image, gray_img, CV_BGR2GRAY);
+    imwrite("/home/zhaoqiangwei/CLionProjects/opencv_test/bin/result.jpg", gray_img);
+
+    return 0;
+}
