20d63e08106285874b3c82b268ea9282dfae1d49
zhaoqiangwei <zhaoqiangwei2002@163.com>
2021-04-04 00:17:03 +0800

    draw

--- src/draw.cpp
+++ src/draw.cpp
@@ -3,6 +3,11 @@
 //
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <opencv2/imgproc/types_c.h>
 
 
 #include <iostream>
@@ -24,9 +29,9 @@ int Drawing_Random_Lines( Mat image, char* window_name, RNG rng )
     int lineType = 8;
     Point pt1, pt2;
     int x_1 = 10;
-    int x_2 = 100;
+    int x_2 = 1000;
     int y_1 = 5;
-    int y_2 = 50;
+    int y_2 = 900;
     int DELAY = 2;
 
     for( int i = 0; i < NUMBER; i++ )
@@ -41,7 +46,7 @@ int Drawing_Random_Lines( Mat image, char* window_name, RNG rng )
 //        if( waitKey( DELAY ) >= 0 )
 //        { return -1; }
 
-        waitKey( DELAY );
+        waitKey();
     }
     return 0;
 }
@@ -78,6 +83,27 @@ int Displaying_Big_End( Mat image, char* window_name, RNG rng ){
 
 
 int main(int ac, char **av) {
+
+    Mat img = imread("/home/zhaoqiangwei/mygit/com.cplusplus/opencv_test/img1.jpg", 1);
+    Mat grey;
+    cvtColor(img, grey, CV_BGR2GRAY);
+
+    Mat sobelx;
+    Sobel(grey, sobelx, CV_32F, 1, 0);
+
+    double minVal, maxVal;
+    minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
+    Mat draw;
+    sobelx.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal);
+
+    namedWindow("image", CV_WINDOW_AUTOSIZE);
+    imshow("image", draw);
+    waitKey();
+
+    Mat img_tmp = imread("/home/zhaoqiangwei/mygit/com.cplusplus/opencv_test/img1.jpg\"");
+    IplImage img1 = img_tmp;
+    CvMat m = img_tmp;
+
     RNG rng( 0xFFFFFFFF );
 
     int window_height = 600;
